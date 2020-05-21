bcd694f6e7a02f181060af62c32e1e19d6d4f5ab
odysseas <OdysseasKr@users.noreply.github.com>
2020-05-21 21:18:34 +0200

    Add a description on the top of the logfile

--- src/logger.py
+++ src/logger.py
@@ -1,13 +1,19 @@
 import os
 import datetime
 
+
 class Logger:
-    def __init__(self, path):
+    def __init__(self, path, description=""):
         super().__init__()
-        datetime_now = str(datetime.datetime.now()).replace(" ","-")
+        datetime_now = str(datetime.datetime.now()).replace(" ", "-")
         if not os.path.exists(path):
             os.mkdir(path)
-        self.log_file = open(os.path.join(path,datetime_now+".log"), 'w')
+        self.log_file = open(os.path.join(path, datetime_now+".log"), 'w')
+        self.write_description(description)
+
+    def write_description(self, description):
+        self.log_file.write(description+'\n')
+        self.log_file.write("=======================\n")
 
     def write(self, string):
         self.log_file.write(string+'\n')
