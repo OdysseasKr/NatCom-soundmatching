d3952fdd1d112d75fe0e0a4ec63acc0284a6c18f
odysseas <OdysseasKr@users.noreply.github.com>
2020-05-27 01:05:30 +0200

    Switch to JSON logging

--- src/logger.py
+++ src/logger.py
@@ -1,25 +1,56 @@
 import os
 import datetime
+import json
+import numpy as np
 
 
 class Logger:
     def __init__(self, path, description=""):
         super().__init__()
-        datetime_now = str(datetime.datetime.now()).replace(" ", "-")
+        self.date = str(datetime.datetime.now()).replace(" ", "-")
+        self.path = os.path.join(path, self.date + '.json')
         if not os.path.exists(path):
             os.mkdir(path)
-        self.log_file = open(os.path.join(path, datetime_now+".log"), 'w')
-        self.write_description(description)
+        self.log = {'description': description, 'targets': []}
+        self._curr_target = None
 
-    def write_description(self, description):
-        self.log_file.write(description+'\n')
-        self.log_file.write("=======================\n")
+    def set_header(self, header_parameters):
+        for k in header_parameters:
+            self.log[k] = header_parameters[k]
 
-    def write(self, string):
-        self.log_file.write(string+'\n')
+    def set_target(self, target):
+        self._curr_target = len(self.log['targets'])
+        self.log['targets'].append({'target': target, 'runs': []})
 
-    def flush(self):
-        self.log_file.flush()
+    def add_run(self, best, best_fit, n_gens, early_stopping, runtime):
+        if self._curr_target is None:
+            raise AssertionError('No target so far. ' +
+                                 'Can\'t add a new run for an unknown target')
+
+        run_obj = {
+            'best': best,
+            'best_fit': float(best_fit),
+            'n_gens': n_gens,
+            'early_stopping': early_stopping,
+            'runtime': runtime
+        }
+
+        self.log['targets'][self._curr_target]['runs'].append(run_obj)
+
+    def calculate_metrics(self):
+        metrics = {}
+        metrics['mean_fitness'] = self._mean_fitness()
+        # TODO: Add 2 more metrics
+        self.log['metrics'] = metrics
+
+    def _mean_fitness(self):
+        fitness = 0
+        for t in self.log['targets']:
+            target_fitness = [r['best_fit'] for r in t['runs']]
+            fitness += np.mean(target_fitness) / len(self.log['targets'])
+        return fitness
 
     def close(self):
-        self.log_file.close()
+        self.calculate_metrics()
+        with open(self.path, 'w') as outfile:
+            json.dump(self.log, outfile)
--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,14 +1,10 @@
-import os
 import random
 import multiprocessing
 import time
 import numpy as np
-import pandas as pd
 import synth
 from deap import creator, base, tools, algorithms
-import librosa
 import matplotlib.pyplot as plt
-from utils import num_of_digits
 from logger import Logger
 from target import TargetGenerator
 
@@ -29,6 +25,7 @@ if GENE == 'categorical':
 else:
     import binary_ga as ga
 
+
 def run_evolutionary_algorithm(toolbox, n_generations=GENERATIONS, population_size=POP_SIZE,
                                tournament_size=TOURNSIZE, crossover_prob=0.5, mutation_prob=0.1):
     """
@@ -37,44 +34,45 @@ def run_evolutionary_algorithm(toolbox, n_generations=GENERATIONS, population_si
 
     population = toolbox.population(n=population_size)
     fitness_vals = toolbox.map(toolbox.evaluate, population)
-    logger.write(f'Initial population: {population}')
-    logger.write('Mean error of the initial population = {:.3f}\n'.format(np.mean(list(fitness_vals))))
 
     start = time.time()
     for gen in range(1, n_generations+1):
-        logger.write(f'Generation {gen}')
         # Perform crossover (with probability cxpb) and mutation (with probability mutpb)
         offspring = algorithms.varAnd(population, toolbox, cxpb=crossover_prob, mutpb=mutation_prob)
-        logger.write(f'\tOffspring (after crossover and mutation): {offspring}')
         # Calculate fitness on the offspring
         fitness_vals = list(toolbox.map(toolbox.evaluate, offspring))
-        logger.write(f'\tFitness values: {[fit[0] for fit in fitness_vals]}')
         mean_fitness = 0
         for fit, ind in zip(fitness_vals, offspring):
             # Set each individuals fitness manually
             ind.fitness.values = fit
             mean_fitness += fit[0]/len(population)
-        logger.write('\t{:20s} {:5.3f}'.format('Mean error:', np.mean(fitness_vals)))
-        logger.write('\t{:20s} {:5.3f}'.format('Error of best:', np.min(fitness_vals)))
 
         # If converged
         if np.min(fitness_vals) < 0 + EPSILON:
-            logger.write("Generation converged!")
             population = offspring
             break
 
         population = toolbox.select(offspring, k=population_size)
-        logger.write(f'\tNew population after selection: {population}')
+    run_time = time.time() - start
 
-    logger.write('Total runtime: {:.2f} seconds'.format(time.time()-start))
-    logger.flush()
-    return tools.selBest(population, k=1)
+    return tools.selBest(population, k=1)[0], gen, run_time
 
 if __name__ == '__main__':
+    # How many signals to approximate
+    logger = Logger('../logs')
+    logger.set_header({
+        'seed': SEED,
+        'epsilon': EPSILON,
+        'gene': GENE,
+        'pop_size': POP_SIZE,
+        'max_gen': GENERATIONS,
+        'tourn_size': TOURNSIZE,
+        'n_targets': N_TARGETS,
+        'n_runs': N_RUNS
+    })
+
     # Set seed for reproducibility
     random.seed(SEED)
-    # How many signals to approximate
-    logger = Logger("../logs")
 
     # Define fitness function objective (minimisation)
     creator.create('FitnessMin', base.Fitness, weights=(-1.0,))
@@ -98,7 +96,7 @@ if __name__ == '__main__':
         target_params, target_sound = next(target_generator)
         target_features = ga.extract_features(target_sound)
 
-        logger.write(f'TARGET {i+1}: {target_params}')
+        logger.set_target(target_params)
         target_params_list.append(list(target_params.values()))
         target_sounds.append(target_sound)
 
@@ -107,17 +105,19 @@ if __name__ == '__main__':
         for n in range(N_RUNS):
             target_params_list.append(list(target_params.values()))
             target_sounds.append(target_sound)
-            best_individual = run_evolutionary_algorithm(toolbox)[0]
+            best_individual, gens, runtime = run_evolutionary_algorithm(toolbox)
+
             best_individuals.append(best_individual)
             best_fitnesses.append(ga.fitness(best_individual, target_features)[0])
+            logger.add_run(best=ga.individual_to_params(best_individual),
+                           best_fit=ga.fitness(best_individual, target_features)[0],
+                           n_gens=gens,
+                           early_stopping=(gens < GENERATIONS),
+                           runtime=runtime)
 
-    logger.write(f'\nAll targets:                    {target_params_list}')
-    logger.write(f'Final predictions per target:   {best_individuals}')
-    logger.write(f'Best fitness values per target: {best_fitnesses}')
     logger.close()
-
     # Plot all predictions vs. target (for debugging)
-    for i,best_individual in enumerate(best_individuals):
+    for i, best_individual in enumerate(best_individuals):
         plt.grid(True, zorder=0)
         plt.plot(target_sounds[i][:300], label='Target', zorder=2)
         # Plot predicted signal
