be6a6c14aeb809e2ba385c67a97293cafcbfbbde
odysseas <OdysseasKr@users.noreply.github.com>
2020-05-27 02:09:24 +0200

    Add more metrics

--- src/logger.py
+++ src/logger.py
@@ -40,7 +40,8 @@ class Logger:
     def calculate_metrics(self):
         metrics = {}
         metrics['mean_fitness'] = self._mean_fitness()
-        # TODO: Add 2 more metrics
+        metrics['proportion_of_early_stopping'] = self._proportion_of_early_stopping()
+        metrics['fitness_evaluations_per_run'] = self._evaluations_per_run()
         self.log['metrics'] = metrics
 
     def _mean_fitness(self):
@@ -49,6 +50,21 @@ class Logger:
             target_fitness = [r['best_fit'] for r in t['runs']]
             fitness += np.mean(target_fitness) / len(self.log['targets'])
         return fitness
+
+    def _proportion_of_early_stopping(self):
+        total = 0
+        for t in self.log['targets']:
+            targen_prop = [r['early_stopping'] for r in t['runs']]
+            total += (np.sum(targen_prop) / len(t['runs'])) / len(self.log['targets'])
+        return total
+
+    def _evaluations_per_run(self):
+        mean_n_gens = 0
+        for t in self.log['targets']:
+            targen_n_gens = [r['n_gens'] for r in t['runs']]
+            mean_n_gens += np.mean(targen_n_gens) / len(self.log['targets'])
+
+        return mean_n_gens * self.log['pop_size']
 
     def close(self):
         self.calculate_metrics()
