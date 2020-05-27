51b47065a965a1dd34a82a7141407323d35a4c8f
odysseas <OdysseasKr@users.noreply.github.com>
2020-05-27 02:12:27 +0200

    Move filter in the synth module

--- src/synth.py
+++ src/synth.py
@@ -3,8 +3,11 @@ import numpy as np
 import itertools
 from synthplayer.oscillators import Sine, Triangle, Square, SquareH, Sawtooth
 from synthplayer.oscillators import Pulse, WhiteNoise, Semicircle, MixingFilter
+from synthplayer.oscillators import Filter, Oscillator
 from synthplayer import params
-from filters import LowPassFilter
+from typing import Generator, List
+from scipy.signal import butter, filtfilt
+
 
 osc_1_options = {
     'Sine': Sine,
@@ -95,3 +98,24 @@ class Synth(ABC):
         """
         self._hookup_modules(note)
         return self._get_raw_data_from_obj(self.out, duration)
+
+
+class LowPassFilter(Filter):
+    """
+    A low pass filter applied to an oscillator with the given cutoff
+    """
+    def __init__(self, source: Oscillator, cutoff: float, samplerate: float) -> None:
+        assert isinstance(source, Oscillator)
+        super().__init__([source])
+        self.cutoff = cutoff
+        self.samplerate
+
+    def blocks(self) -> Generator[List[float], None, None]:
+        source_blocks = self.sources[0].blocks()
+        try:
+            while True:
+                block = next(source_blocks)
+                b, a = butter(4, self.cutoff / (self.samplerate / 2.), 'low')
+                yield filtfilt(b, a, block)
+        except StopIteration:
+            return
